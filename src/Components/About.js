f5277739a31632eb56f9fb327d53c4402c9517b8
Carolyn Gregoris <carolyn.a.scobie@gmail.com>
2022-02-10 17:55:24 -0700

    spell check

--- src/Components/About.js
+++ src/Components/About.js
@@ -17,7 +17,7 @@ function About() {
         <div className="right-side--about">
           <h3>About MoodReads</h3>
           <p>
-            My love for reading inspired me to create this app. There are many tools designed to help find a book to read - for example, the GoodReads app which I rely on heaily. While these resources are good for finding books based on genre or titles similar to those you have already read, they lack a personal, and less-specific element. Sometimes you are looking for a book that matches a certain 'vibe'. MoodReads recreates the experience of asking for book recommendations from a well-read friend. Instead of searching for a book by genre, our app enables you to find a new read by emotion. 
+            My love for reading inspired me to create this app. There are many tools designed to help find a book to read - for example, the GoodReads app which I rely on heavily. While these resources are good for finding books based on genre or titles similar to those you have already read, they lack a personal, and less-specific element. Sometimes you are looking for a book that matches a certain 'vibe'. MoodReads recreates the experience of asking for book recommendations from a well-read friend. Instead of searching for a book by genre, our app enables you to find a new read by emotion. 
           </p>
           <div className="abt">
             <h4>Created by:</h4>
